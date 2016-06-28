974130cec7a74409ab3b4454e0c45350de6b44b3
akinobu.fujii <aki.develop@gmail.com>
2016-06-29 00:39:11 +0900

    FPS表示するように対応しました

--- 00_Skelton/00_Skelton.cpp
+++ 00_Skelton/00_Skelton.cpp
@@ -1,20 +1,45 @@
 #define GLFW_INCLUDE_GLU
+#define GLFW_EXPOSE_NATIVE_WIN32
+#define GLFW_EXPOSE_NATIVE_WGL
 
 #if defined(_WIN32)
 #pragma comment(lib, "OpenGL32.lib")
 #endif
 
+#include <tchar.h>
+#include <cmath>
+#include <string>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
+
+#include "../utility/FPSCounter.h"
 
 static const char*	WINDOW_TITLE		= "00_Skelton";	// ウィンドウ名
 static const bool	IS_FULLSCREEN_MODE	= false;		// フルスクリーンモード（trueでフルスクリーン）
 static const int	WINDOW_WIDTH		= 1280;			// ウィンドウの横幅
 static const int	WINDOW_HEIGHT		= 720;			// ウィンドウの縦幅
 
-// エントリーポイント
-int main()
+//==============================================================================
+// 描画
+//==============================================================================
+void Render()
 {
+	static float count = 0;
+	count += 0.0001f;
+	count = fmodf(count, 1.0f);
+
+	glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(0, count, 1, 1);
+}
+
+//==============================================================================
+// エントリーポイント
+//==============================================================================
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	glewExperimental = GL_TRUE;
+
 	// GLFW初期化
 	glfwInit();
 
@@ -32,14 +57,22 @@ int main()
 	// 現在のウィンドウをスレッドに渡す
 	glfwMakeContextCurrent(pWindow);
 
+	FPSCounter fpsCounter; // FPSカウンター
+
 	// OpenGL描画ループ
 	while(glfwWindowShouldClose(pWindow) == 0)
 	{
-		glClear(GL_COLOR_BUFFER_BIT);
-		glClearColor(0, 0, 1, 1);
+		fpsCounter.beginCount();
+
+		Render();
 
 		glfwSwapBuffers(pWindow);
 		glfwPollEvents();
+
+		fpsCounter.endCount();
+
+		std::string windowTitle = std::string(WINDOW_TITLE) + " - " + std::to_string(fpsCounter.getLastFPS()) + " FPS";
+		SetWindowText((glfwGetWin32Window(pWindow)), windowTitle.c_str());
 	}
 
 	// GLFW終了
